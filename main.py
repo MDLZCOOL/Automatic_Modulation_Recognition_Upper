d48c8b4443022259d70d13e3d99c83ce4da1f6fc
MDLZCOOL <mdlzcool@mail.nwpu.edu.cn>
2025-04-09 22:04:13 +0800

    fix: import standardize

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import sys
 import asyncio
+import PySide6.QtAsyncio as QtAsyncio
 
-from PySide6.QtGui import QGuiApplication
 from PySide6.QtWidgets import QApplication
 from PySide6.QtQml import QQmlApplicationEngine, QmlElement
-from PySide6.QtCore import QTimer, QObject, Signal, Slot
-import PySide6.QtAsyncio as QtAsyncio
 from time import strftime, localtime
 
-import data_source
 
 async def sleep(seconds: int):
 	"""Asynchronous sleep function"""
