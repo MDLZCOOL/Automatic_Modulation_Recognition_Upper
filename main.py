12b89dd1f7ec1b7a55eb456ca75654c664f75aea
MDLZCOOL <mdlzcool@mail.nwpu.edu.cn>
2025-04-09 22:42:07 +0800

    fix: 修复fetal err

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 import asyncio
 import PySide6.QtAsyncio as QtAsyncio
+import data_source
 
 from PySide6.QtWidgets import QApplication
 from PySide6.QtQml import QQmlApplicationEngine, QmlElement
 from time import strftime, localtime
+from PySide6.QtCore import QTimer, QObject, Signal, Slot
+from PySide6.QtGui import QGuiApplication
 
 
 async def sleep(seconds: int):
